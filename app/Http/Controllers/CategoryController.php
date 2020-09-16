a7f07c78ee3fe382cb43a816f3812cfcebe42c32
Jose C <josecarvalholdjr@gmail.com>
2020-09-16 18:36:17 +0000

    Show categoriy

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,29 +7,31 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+    private $rules = [
+        'name' => 'required|max:255]',
+        'is_active' => 'boolean'
+    ];
+
     public function index()
     {
         return Category::all();
     }
 
     public function create(Request $request)
-    { 
+    {
     }
-    
+
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:255]',
-            'is_active' => 'boolean'            
-        ]);
+        $this->validate($request, $this->rules);
 
-        return Category::create($request->all());
-        //
+        return Category::create($request->all());    
     }
 
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     public function edit(Category $category)
@@ -39,11 +41,15 @@ class CategoryController extends Controller
 
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+
+        return $category;
     }
- 
+
     public function destroy(Category $category)
-    {
-        //
+    {        
+        $category->delete();
+        return response()->noContent(); // 204 - No Content
     }
 }
