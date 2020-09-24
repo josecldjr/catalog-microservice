70d6a5007a32e13b747c77769aabc44c74a650e3
josecldjr <josecarvalholdjr@gmail.com>
2020-09-24 20:18:29 +0000

    Uuid for genres

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid as TraitsUuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Ramsey\Uuid\Uuid;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, TraitsUuid;
+
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $casts = [
+        'id' => 'string'
+    ];
 }
