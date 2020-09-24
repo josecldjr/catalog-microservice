ea65b7b98dd29d0c406d675b0df632f0b5480da3
josecldjr <josecarvalholdjr@gmail.com>
2020-09-24 19:58:24 +0000

    Soft delete for categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class Category extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['name', 'description', 'is_active'];
+    protected $dates = ['deleted_at'];
 }
--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+$exceptCreateAndEdit = ['except' => ['create', 'edit']];
 
 // Route::group(['namespace' => 'Api'], function () {
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-    
+Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
+// Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
+  
     
 // });
