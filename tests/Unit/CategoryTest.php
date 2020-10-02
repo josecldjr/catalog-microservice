42b217f0243d97cfb49fdf4006e716a3b052758c
josecldjr <josecarvalholdjr@gmail.com>
2020-10-02 18:34:58 +0000

    Category tests

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CategoryTest extends TestCase
+{
+
+    public function testFillable()
+    {
+        $category = new Category();
+        $fillable = ['name', 'description', 'is_active'];
+        $this->assertEquals(
+            $fillable,
+            $category->getFillable()
+        );
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class,
+            Uuid::class
+        ];
+
+        $classTraits = array_values(class_uses(Category::class));
+
+        $this->assertEquals($traits, $classTraits);
+    }
+
+    public function testCasts()
+    {
+        $casts = ['id' => 'string'];
+        $category = new Category();
+
+        $this->assertEquals($casts, $category->getCasts());
+    }
+
+    public function testDates()
+    {
+        $dates = ['deleted_at', 'created_at',  'updated_at'];
+        $category = new Category();
+
+        foreach ($dates as $date) {
+            $this->assertContains($date, $category->getDates());
+        }
+    }
+
+    public function testIncrementing()
+    {
+        $category = new Category();
+
+        $this->assertFalse($category->incrementing);
+    }
+}
